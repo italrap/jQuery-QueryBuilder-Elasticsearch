f833c01668d30a135eae11a8b4699f9b3868628f
Davide Graziosi <davide.graziosi@exprivia.it>
2017-03-30 10:54:57 +0200

    commented out getESQueryStringQuery managed empty esquery date fields : added Timezone and formatting added new operators: last_n_minutes, before_last_n_minutes, period operators in and not_in : can now use multiselect

--- query-builder-elasticsearch.js
+++ query-builder-elasticsearch.js
@@ -21,31 +21,71 @@
     // DEFAULT CONFIG
     // ===============================
     QueryBuilder.defaults({
+    
+    	DateExpressions: {
+	        'NOW' : 'now',
+		'TRUNC(NOW)': 'now/d',
+		'TRUNC(NOW)-1': 'now/d-1d',
+		'TRUNC(NOW)-30': 'now/d-30d',
+		'TRUNC(NOW)-1 second' : 'now/d-1s',
+		'TRUNC(ADD_MONTHS(NOW, -1),MM)' : 'now-1M/M',
+		'TRUNC(NOW,MM)-1 second' : 'now/M-1s',
+		'TRUNC(NOW,IW)' : 'now/w',
+		'TRUNC(TO_DATE(NOW),IW)+7-1 second' : 'now/w+7d-1s'
+		/*'NOW - 1' : 'now-1d',
+		'NOW - 2' : 'now-2d',
+		'NOW - 7' : 'now-7d',
+		'NOW - 30' : 'now-30d',
+	    	'NOW - 1 minute' : 'now-1m',
+	    	'NOW - 300 minute' : 'now-300m',
+	    	'NOW - 120 minute' : 'now-120m',
+	    	'NOW - 60 minute' : 'now-60m',
+	    	'NOW - 600 minute': 'now-600m'*/	
+	},
         ESBoolOperators: {
-            equal:            function(v){ return v; },
-            not_equal:        function(v){ return v; },
+            is_empty:         function(){ return "term"; },
+            is_null:          function(){ return "exists"; },
+            is_not_empty:     function(){ return "term"; },
+            is_not_null:      function(){ return "exists"; },
+            contains:         function(v){ return v.toLowerCase(); },
+   	    not_contains:     function(v){ return v.toLowerCase(); },
+            equal:            function(v){ return v.toLowerCase(); },
+            not_equal:        function(v){ return v.toLowerCase(); },
+	    begins_with:      function(v){ return v.toLowerCase(); },
+	    ends_with:      function(v){ return v.toLowerCase(); },
+	    not_begins_with:      function(v){ return v.toLowerCase(); },
+	    not_ends_with:      function(v){ return v.toLowerCase(); },
             less:             function(v){ return {'lt': v}; },
             less_or_equal:    function(v){ return {'lte': v}; },
             greater:          function(v){ return {'gt': v}; },
             greater_or_equal: function(v){ return {'gte': v}; },
             between:          function(v){ return {'gte': v[0], 'lte': v[1]}; },
-            in :              function(v){ return v.split(',').map(function(e) { return e.trim();}); },
-            not_in:           function(v){ return v.split(',').map(function(e) { return e.trim();}); },
-            is_null:          function(v){ return v; },
-            is_not_null:      function(v){ return v; }
-        },
-        ESQueryStringQueryOperators: {
-            is_not_null:           function(){ return "_exists_:"; },
-            is_null:          function(){ return "_missing_:";},
-            contains:         function(v){ return v; },
-            between:          function(v){ return '[' + v[0] + ' TO '+ v[1] + "]"; },
-        }
+	    not_between:      function(v){ return {'gte': v[0], 'lte': v[1]}; },
+            in :              function(v){ if (typeof v === 'string') return v.split(',').map(function(e) { return e.trim().toLowerCase();});
+            							  else return v.map(function(e) { return e.trim().toLowerCase();}); },
+            not_in:           function(v){ if (typeof v === 'string') return v.split(',').map(function(e) { return e.trim().toLowerCase();});
+            							   else return v.map(function(e) { return e.trim().toLowerCase();}); },
+	        last_n_minutes:   function(v){ return {'gte': v[0], 'lte': v[1]}; },
+	        period:           function(v){ return {'gte': v[0], 'lte': v[1]}; },
+	        before_last_n_minutes:   function(v){ return {'lt': v}; }
+        }, 
+	ESBoolDateOperators: {
+            equal:            function(v){ return {'lte': v, 'gte': v, 'format' : 'yyyy-MM-dd HH:mm:ssZ'}; },
+            not_equal:        function(v){ return {'lte': v, 'gte': v, 'format' : 'yyyy-MM-dd HH:mm:ssZ'}; },
+	    less:             function(v){ return {'lt': v , 'format' : 'yyyy-MM-dd HH:mm:ssZ'}; },
+            less_or_equal:    function(v){ return {'lte': v, 'format' : 'yyyy-MM-dd HH:mm:ssZ'}; },
+            greater:          function(v){ return {'gt': v, 'format' : 'yyyy-MM-dd HH:mm:ssZ'}; },
+            greater_or_equal: function(v){ return {'gte': v, 'format' : 'yyyy-MM-dd HH:mm:ssZ'}; },
+            between:          function(v){ return {'gte': v[0], 'lte': v[1], 'format' : 'yyyy-MM-dd HH:mm:ssZ'}; },
+	    not_between:      function(v){ return {'gte': v[0], 'lte': v[1], 'format' : 'yyyy-MM-dd HH:mm:ssZ'}; },
+	}
     });
-
+  
 
     // PUBLIC METHODS
     // ===============================
     QueryBuilder.extend({
+	
         /**
         * Get rules as an elasticsearch bool query
         * @param data {object} (optional) rules
@@ -57,6 +97,10 @@
             var that = this;
 
             return (function parse(data) {
+		if (!data || !data.rules) {
+                    return {};
+                }
+
                 if (!data.condition) {
                     data.condition = that.settings.default_condition;
                 }
@@ -66,10 +110,6 @@
                         'Unable to build Elasticsearch bool query with condition "{0}"'
                         .replace('{0}', data.condition)
                     );
-                }
-
-                if (!data.rules) {
-                    return {};
                 }
 
                 var parts = {};
@@ -84,10 +124,35 @@
                         if (rule.data && rule.data.hasOwnProperty('transform')) {
                             return window[rule.data.transform].call(this, rule.value);
                         } else {
+			    if (rule.operator === 'begins_with' || rule.operator === 'not_begins_with') return rule.value+".*";
+			    if (rule.operator === 'ends_with' || rule.operator === 'not_ends_with') return ".*"+rule.value;
+			    if (rule.operator === 'contains' || rule.operator === 'not_contains') return ".*"+rule.value+".*";
+                            if (rule.operator === 'is_empty' || rule.operator === 'is_not_empty') return "";
                             return rule.value;
                         }
                     }
 
+                    function transformDateExpression(value) {
+            			var transfVal = that.settings.DateExpressions[value] || value;
+            			
+            			var minutes = /^NOW - (\d+) minute$/.exec(value);
+            			var days = /^NOW - (\d+)$/.exec(value);
+            			if (minutes) return "now-"+minutes[1]+"m";
+            			if (days) return "now-"+days[1]+"d";
+            			
+            			if (/^\d{4}-\d{2}-\d{2}/.exec(value)) transfVal = addTimezoneToDate(transfVal);
+            			
+            			return transfVal;
+            		}
+		    
+		   function addTimezoneToDate (value) {
+			var myDate = value.replace(/-/g, "/");
+			var dateValue = new Date(Date.parse(myDate));
+			var numberformatter = new Intl.NumberFormat('it', { minimumIntegerDigits: 2 });
+			var timezoneoffset = -1 * dateValue.getTimezoneOffset() / 60;
+			return value + (dateValue.getTimezoneOffset()<0?"+":"")+numberformatter.format(timezoneoffset)+":00";
+		   }
+                    
                     function make_query(rule) {
                         var mdb = that.settings.ESBoolOperators[rule.operator],
                         ope = that.getOperatorByType(rule.operator),
@@ -102,17 +167,50 @@
 
                         if (ope.nb_inputs !== 0) {
                             var es_key_val = {};
-                            es_key_val[rule.field] =  mdb.call(that, get_value(rule));
-                            part[getQueryDSLWord(rule)] = es_key_val;
-                        }
-
-                        if (rule.operator === 'is_null' || rule.operator === 'is_not_null') {
-                            part = {exists: {field: rule.field }};
-                        }
+                            if ( /^date/.exec(rule.type) ) {
+				var useterm,useterms = "";
+				/*if (/.custom$/.exec(rule.field) ) {
+	    			rule.field = rule.field.replace(".custom", '');*/
+					var myDate = get_value(rule);
+					var _myDates ;
+					if (Array.isArray(myDate)) {
+					   _myDates = [];
+					   myDate.forEach(function(value, index) {
+						   _myDates[index] = transformDateExpression(value); 
+					   });
+					}else {
+						_myDates = transformDateExpression(myDate);
+					}
+					
+					if (rule.operator in that.settings.ESBoolDateOperators) {
+						  mdb = that.settings.ESBoolDateOperators[rule.operator];
+					}
+			        es_key_val[rule.field] =  mdb.call(that, _myDates);
+				    part[getQueryDSLWord(rule, true)] = es_key_val;
+				    
+				/*}*/
+				
+			    } else {
+			        es_key_val[rule.field] =  mdb.call(that, get_value(rule));
+			        part[getQueryDSLWord(rule)] = es_key_val;                              
+			    }
+                        } 
+			else {
+			    var es_key_val = mdb.call(that, rule.value);
+			    var val = {};
+                            if (es_key_val === 'exists') {
+                            	 val["field"] = rule.field ;
+			    } else if (es_key_val === 'term') {
+				 val[rule.field] = get_value(rule);
+                            }
+			    part[es_key_val] = val;
+                        } 
 
                         // this is a corner case, when we have an "or" group and a negative operator,
                         // we express this with a sub boolean query and must_not.
-                        if (data.condition === 'OR' && (rule.operator === 'not_equal' || rule.operator === 'not_in' || rule.operator === 'is_null')) {
+                        if (data.condition === 'OR' && (rule.operator === 'not_equal' || rule.operator === 'not_in' 
+                                                || rule.operator === 'not_contains'   || rule.operator === 'not_begins_with'   
+                                                || rule.operator === 'not_ends_with'  || rule.operator === 'is_null' )) {
                             return {'bool': {'must_not': [part]}}
                         } else {
                             return part
@@ -132,14 +230,16 @@
                 delete parts.add;
                 return {'bool': parts}
             }(data));
-        },
+        }
 
         /**
         * Get rules as an elasticsearch query string query
         * @param data {object} (optional) rules
         * @return {object}
         */
-        getESQueryStringQuery: function(data) {
+        
+        /*
+        ,getESQueryStringQuery: function(data) {
             data = (data===undefined) ? this.getRules() : data;
 
             var that = this;
@@ -207,20 +307,24 @@
                 });
                 return parts;
             }(data));
-        }
+        }*/
     });
 
     /**
     * Get the right type of query term in elasticsearch DSL
     */
-    function getQueryDSLWord(rule) {
-        var term = /^(equal|not_equal)$/.exec(rule.operator),
+    function getQueryDSLWord(rule, isDate) {
+        var term = /^(equal|not_equal|is_empty|is_not_empty)$/.exec(rule.operator),
             wildcard = /.(\*|\?)/.exec(rule.value),
-            terms = /^(in|not_in)$/.exec(rule.operator);
+            terms = /^(in|not_in)$/.exec(rule.operator),
+	    matchs = /^(contains|not_contains)$/.exec(rule.operator),
+	    begins_ends = /.*(begins_with|ends_with)$/.exec(rule.operator);
 
         if (term !== null && wildcard !== null) { return 'wildcard'; }
-        if (term !== null) { return 'term'; }
+        if (term !== null) { return (!isDate ? 'term' : 'range'); } //TODO riportare le modifiche nel altro progetto
         if (terms !== null) { return 'terms'; } 
+        if (matchs !== null) { return 'regexp'; }
+        if (begins_ends !== null) { return 'regexp'; }
         return 'range';
     }
 
@@ -228,9 +332,16 @@
     * Get the right type of clause in the bool query
     */
     function getClauseWord(condition, operator) {
-        if (condition === 'AND' && (operator !== 'not_equal' && operator !== 'not_in' && operator !== 'is_null')) { return 'must' }
-        if (condition === 'AND' && (operator === 'not_equal' || operator == 'not_in' || operator === 'is_null')) { return 'must_not' }
+        if (condition === 'AND' && (operator !== 'not_equal' && operator !== 'not_in' && operator !== 'not_contains'
+                                     && operator !== 'not_begins_with' && operator !== 'not_ends_with' 
+                                     && operator !== 'is_null' && operator !== 'is_not_empty' 
+                                     &&  operator !== 'not_between')) { return 'must' }
+        if (condition === 'AND' && (operator === 'not_equal' || operator === 'not_in' || operator === 'not_contains'
+				    || operator === 'not_begins_with' || operator === 'not_ends_with' || 
+                                     operator === 'is_null' || operator === 'is_not_empty' || 
+                                     operator === 'not_between')) { return 'must_not' }
         if (condition === 'OR') { return 'should' }
     }
 
 }));
+
